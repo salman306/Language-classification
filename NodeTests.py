16c809612aa2ff7f36e0cb45af531ff22a9db234
niklasbrake <niklas.brake@mail.mcgill.ca>
2017-10-14 14:55:24 -0400

    Decision Tree Tests
    Allows the creation of a test function as a class. Execute that test
    function on the data and get the left/right split and the conditional
    entropy

---
+++ NodeTests.py
@@ -0,0 +1,56 @@
+from math import log
+import numpy as np
+
+class Test(object):
+    """docstring for Test
+    column is the feature on which the test is performed.
+    test is either -2,-1,0,1,2 which is translated into <,<=,=,>=,> respectively
+    critValue is the value on which the data is split as per the specific test"""
+    def __init__(self, column, test, critValue):
+        self.column = column
+        self.test = test
+        self.critValue = critValue
+
+    def splitData(self,X):
+        if(self.test == -2):
+            sortLabels = X[:,self.column] < self.critValue 
+        elif(self.test == -1):
+            sortLabels = X[:,self.column] <= self.critValue 
+        elif(self.test == 0):
+            sortLabels = X[:,self.column] = self.critValue 
+        elif(self.test == 1):
+            sortLabels = X[:,self.column] >= self.critValue 
+        elif(self.test == 2):
+            sortLabels = X[:,self.column] > self.critValue 
+
+        return sortLabels
+
+
+def evalTest(X,Y,Test,classCount):
+
+    sampleCount = len(Y)
+
+    sortLabels = Test.splitData(X)
+
+    HA = 0
+    HB = 0
+
+    nA = sum(sortLabels)
+    nB = sampleCount - nA
+
+    for y in range(0,classCount):
+        pA = sum([a == y and b for a, b in zip(Y,sortLabels)])
+        pB = sum([a == y and not b for a, b in zip(Y,sortLabels)])
+        if(pA == 0):
+            HA += 0
+        else:
+            HA += -pA/nA*log(pA/nA)/log(2)
+        if(pB == 0):
+            HB += 0
+        else:
+            HB += -pB/nB*log(pB/nB)/log(2)
+
+
+    CondEntropy = (HA/nA + HB/nB)/sampleCount
+
+    return [sortLabels,CondEntropy]
