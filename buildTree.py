94f8f8330171fa324d7a700e4e79a93abc5bcc8b
niklasbrake <niklas.brake@mail.mcgill.ca>
2017-10-16 12:42:05 -0400

    Random Tree code
    Constructs decision tree with random parameters chosen for test funciton
    at each node

--- buildTree.py
+++ buildTree.py
@@ -1,65 +1,42 @@
 import numpy as np
+import random
 from math import log
 
 class Node(object):
     """docstring for Node"""
-    def __init__(self, X = None, Y = None, Label = None):
+    def __init__(self, X = None, Y = None, k = None, Label = None):
         self.leftChild = None
         self.rightChild = None
         self.parent = None
         self.Label = Label
         if(Label == None):
-            self.F, self.div = findBestF(X,Y)
+            self.Thresh, self.feature, self.div = findBestF(X,Y,k)
         else:
             self.F = None
             self.div = None
 
-    def test(X):
-        sortLabels = self.F.splitData(X)
+    def test(self,X):
+        sortLabels = X[:,self.feature] < self.Thresh
         return sortLabels
 
-class Test(object):
-    """docstring for Test
-    column is the feature on which the test is performed.
-    test is either -2,-1,0,1,2 which is translated into <,<=,=,>=,> respectively
-    critValue is the value on which the data is split as per the specific test"""
-    def __init__(self, column, test, critValue):
-        self.column = column
-        self.test = test
-        self.critValue = critValue
-
-    def splitData(self,X):
-        if(self.test == -2):
-            sortLabels = X[:,self.column] < self.critValue 
-        elif(self.test == -1):
-            sortLabels = X[:,self.column] <= self.critValue 
-        elif(self.test == 0):
-            sortLabels = X[:,self.column] = self.critValue 
-        elif(self.test == 1):
-            sortLabels = X[:,self.column] >= self.critValue 
-        elif(self.test == 2):
-            sortLabels = X[:,self.column] > self.critValue 
-
-        return sortLabels
-
-
-def buildTree(X,Y,classCount):
+def buildTree(X,Y,classCount,k):
     for i in range(0,classCount):
         if(sum(abs(Y-i)) == 0):
             return Node(Label=i)
-    n = Node(X,Y)
-    n.leftChild = buildTree(X[n.div],Y[n.div],classCount)
-    n.rightChild = buildTree(X[~n.div],Y[~n.div],classCount)
+    n = Node(X,Y,k) 
+    n.leftChild = buildTree(X[n.div],Y[n.div],classCount,k)
+    n.rightChild = buildTree(X[~n.div],Y[~n.div],classCount,k)
     n.leftChild.parent = n
     n.rightChild.parent = n
     return n
 
-def findBestF(X,Y):
+def findBestF(X,Y,k):
 
     classCount = 5
 
     a,b = np.shape(X)
-    A = np.sort(X,0)
+    J = random.sample(range(b),k)
+    A = np.sort(X[:,J],0)
 
     Thresholds = (A[:-1,:] + A[1:,:])/2
 
@@ -67,45 +44,60 @@ def findBestF(X,Y):
     bestF = []
     bestDiv = []
 
-    for i in range(0,a-1):
-        for j in range(0,b):
-            F = Test(j,-1,Thresholds[i,j])
+    for i in range(a-1):
+        for j in range(k):
+            sortLabels = X[:,J[j]] < Thresholds[i,j]
 
-            labels, H = evalTest(X,Y,F,classCount)
+            HA = 0
+            HB = 0
+
+            nA = sum(sortLabels)
+            nB = a - nA
+
+            # Calculate entropy
+            for y in range(0,classCount):
+                pA = sum([a == y and b for a, b in zip(Y,sortLabels)])
+                pB = sum([a == y and not b for a, b in zip(Y,sortLabels)])
+                if(pA == 0):
+                    HA += 0
+                else:
+                    HA += -pA/nA*log(pA/nA)/log(2)
+                if(pB == 0):
+                    HB += 0
+                else:
+                    HB += -pB/nB*log(pB/nB)/log(2)
+
+            H = (HA*nA + HB*nB)/a
+
             if(H < minH):
-                bestF = F
+                bestT = Thresholds[i,j]
+                bestF = J[j]
                 minH = H
-                bestDiv = labels
+                bestDiv = sortLabels
+    return bestT, bestF, bestDiv
 
-    return F, bestDiv
-
-def evalTest(X,Y,Test,classCount):
-
-    sampleCount = len(Y)
-
-    # sortLabels are false if sample failed test and true o.w.
-    sortLabels = Test.splitData(X)
-
-    HA = 0
-    HB = 0
-
-    nA = sum(sortLabels)
-    nB = sampleCount - nA
-
-    # Calculate entropy
-    for y in range(0,classCount):
-        pA = sum([a == y and b for a, b in zip(Y,sortLabels)])
-        pB = sum([a == y and not b for a, b in zip(Y,sortLabels)])
-        if(pA == 0):
-            HA += 0
+def DFS(N):
+    if(N.leftChild == None):
+        if(N.rightChild == None):
+            return 1
         else:
-            HA += -pA/nA*log(pA/nA)/log(2)
-        if(pB == 0):
-            HB += 0
+            n1 = DFS(N.rightChild)
+    elif(N.rightChild == None):
+        if(N.leftChild == None):
+            return 1
         else:
-            HB += -pB/nB*log(pB/nB)/log(2)
+            n2 = DFS(N.rightChild)
+    elif(N.leftChild != None and N.rightChild != None):
+        n1 = DFS(N.rightChild)
+        n2 = DFS(N.leftChild)
+    return n1 + n2 + 1
 
-    # Calculate conditional entropy for passing/failing test
-    CondEntropy = (HA*nA + HB*nB)/sampleCount
-
-    return [sortLabels,CondEntropy]
+def treeClassify(A,N):
+    if(N.Label == None):
+        t = N.test(A)
+        if(t == 0):
+            return treeClassify(A,N.leftChild)
+        else:
+            return treeClassify(A,N.rightChild)
+    else:
+        return N.Label
