1d5b3c8fe3e969c9736bed036dfc8285ab800f16
salman306 <salman306@gmail.com>
2017-10-21 06:21:55 -0400

    Description in comments
    Input would be "finalform" i.e. clean utterance array which we feed to the tfidfvectorizer and target categories

---
+++ randomizer.py
@@ -0,0 +1,23 @@
+import random
+
+def randomconverter(cleanstrings, targets):
+
+    # inputs are array of cleaned utterances and their target categories
+
+    newutt = []
+    newtarget= []
+    for count in range(len(cleanstrings)):
+        if (len(cleanstrings[count])>31):
+            noofnew = 3  # number of new utterances to create from large strings
+            noofelements = 30  # length of new randomly generated utterances
+
+        else:
+            #for shorter utterances, just randomize them without creating additional ones
+
+            noofnew = 1
+            noofelements = len(cleanstrings[count])
+
+        for count2 in range(noofnew):
+            newutt.append(''.join(random.sample((cleanstrings[count]), noofelements)))
+            newtarget.append(targets[count])
+    return (newutt, newtarget)
